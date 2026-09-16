36181fd66b28725a0b0d54301964082c1efda744
RIDA FATHIMA <ridafathima2310@gmail.com>
2026-09-16 21:47:47 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int f(int n,int[] dp){
+        if(n==0||n==1||n==2) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=f(n-1,dp)+f(n-2,dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
+    }
+}
