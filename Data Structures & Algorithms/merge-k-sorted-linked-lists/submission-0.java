509f3f32708fdcd7423892e97d7a7c9db79b3593
RIDA FATHIMA <ridafathima2310@gmail.com>
2026-09-20 14:18:15 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                curr.next=l1;
+                l1=l1.next;
+            }
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        if(l1!=null) curr.next=l1;
+        else curr.next=l2;
+        return dummy.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null) return null;
+        ListNode res=null;
+        for(int i=0;i<lists.length;i++){
+            res=merge(res,lists[i]);
+        }
+        return res;
+    }
+}
